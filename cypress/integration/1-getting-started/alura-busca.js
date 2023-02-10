e0279748f12ae10c2499a380a5327e0bcce01752
Mariana Muniz <maewerton60@gmail.com>
2023-02-10 17:37:19 -0300

    criando gitigine

--- cypress/integration/1-getting-started/alura-busca.js
+++
@@ -1,3 +0,0 @@
-describe('alura busca curso', () =>{
-    
-})
--- cypress/integration/3-Linkedin/linkedin.spec.js
+++ cypress/integration/3-Linkedin/linkedin.spec.js
@@ -7,8 +7,14 @@ describe ('Login e registro de usuários',() => {
     it ('verificar mensagens validacao', () => {
         cy.contains('a', 'New to LinkedIn? Join now').click(); 
         cy.contains ('button', 'Agree & Join').click;
-        cy.contains('p', 'Please enter your email address or mobile number.').should('be.visible'); 
+        cy.contains('p class', 'Please enter your email address or mobile number.').should('be.visible'); 
         cy.contains ('button', 'Agree & Join').click;   
-        cy.contains ('p', 'Please enter your password.').should ('be.visible');
+        cy.contains ('p class', 'Please enter your password.').should('be.visible');
     })
+})
+it ('verificar mensagens de email invalido', () => {
+    cy.contains('a', 'New to LinkedIn? Join now').click(); 
+    cy.contains ('button', 'Agree & Join').click;
+    cy.get ('input[placeholder="Email or phone number"]').type('Mariana')
+    cy.contains ('p class', 'Please enter a valid email address or mobile number.').should('be.visible');
 })
